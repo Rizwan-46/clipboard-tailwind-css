d1d3d94ef6640d1458686a00f5db7d387c244e97
Rizwan-46 <133772340+Rizwan-46@users.noreply.github.com>
2026-07-17 21:21:44 +0500

    Images path changed

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        strongCyan: "hsl(176, 66%, 44%)",
+        lightBlue: "hsl(233, 100%, 69%)",
+        darkGrayishBlue: "hsl(210, 10%, 33%)",
+        grayishBlue: "hsl(210, 11%, 66%)",
+      },
+      fontFamily: {
+        sans: ["Overpass", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
